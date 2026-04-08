348a9ee5529f55c5cf0ade6d298af0777c180c10
Veer <veer@example.com>
2026-04-08 19:12:46 +0530

    fix(auth): make firebase authDomain dynamic to resolve postMessage mismatches

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: typeof window !== "undefined" ? window.location.host : (process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "nexid.in"),
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
